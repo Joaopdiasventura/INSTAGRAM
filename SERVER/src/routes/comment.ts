4b11606a7bcf5d7cad51009e6b8a5e98e1cecc7a
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-02-15 23:02:10 -0300

    fix: arrumando a exportação da rota comments

--- SERVER/src/routes/comment.ts
+++ SERVER/src/routes/comment.ts
@@ -3,10 +3,12 @@ import Post from "./comment/post";
 import Get from "./comment/get";
 import Delete from "./comment/delete";
 
-export default async function Comment(app: FastifyInstance): Promise<void> {
+async function Comment(app: FastifyInstance): Promise<void> {
   app.register(Post);
 
   app.register(Get);
 
   app.register(Delete);
 }
+
+export default Comment;
